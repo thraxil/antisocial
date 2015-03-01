26d4d91e652345105f09f297e90e766ea180ad01
Anders Pearson <anders@columbia.edu>
2015-03-01 20:31:22 +0100

    cloudfront

--- antisocial/settings_production.py
+++ antisocial/settings_production.py
@@ -28,13 +28,15 @@ COMPRESS_ROOT = os.path.join(os.path.dirname(__file__), "../media")
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
+AWS_S3_CUSTOM_DOMAIN = "d115djs1mf98us.cloudfront.net"
+AWS_IS_GZIPPED = True
 
 AWS_STORAGE_BUCKET_NAME = "thraxil-antisocial-static-prod"
 AWS_PRELOAD_METADATA = True
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 STATICFILES_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
-S3_URL = 'https://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = 'https://%s.s3.amazonaws.com/media/' % AWS_STORAGE_BUCKET_NAME
+S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
 COMPRESS_ENABLED = True
 COMPRESS_OFFLINE = True
 COMPRESS_ROOT = STATIC_ROOT
