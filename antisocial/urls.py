d0b87f05b57f428862427f0666b1335be9769039
Anders Pearson <anders@columbia.edu>
2015-06-14 12:12:18 +0200

    stats overhaul

--- antisocial/urls.py
+++ antisocial/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include
 from django.contrib import admin
 from django.conf import settings
+from django.views.generic import TemplateView
+
 admin.autodiscover()
 
 urlpatterns = patterns(
@@ -25,6 +27,7 @@ urlpatterns = patterns(
     (r'^accounts/', include('userena.urls')),
     (r'^admin/', include(admin.site.urls)),
     (r'smoketest/', include('smoketest.urls')),
+    (r'^stats/$', TemplateView.as_view(template_name="stats.html")),
     (r'^uploads/(?P<path>.*)$',
      'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
