ef6347350668662c05489c30e42a105d3825df67
Anders Pearson <anders@columbia.edu>
2015-09-12 21:59:09 +0200

    switch over to thraxilsettings

--- antisocial/settings_production.py
+++ antisocial/settings_production.py
@@ -1,55 +1,13 @@
 # flake8: noqa
 from settings_shared import *
+from thraxilsettings.production import common
 import os.path
 
-TEMPLATE_DIRS = (
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
+app = 'antisocial'
+base = os.path.dirname(__file__)
+cloudfront = "d115djs1mf98us.cloudfront.net"
 
-MEDIA_ROOT = '/var/www/antisocial/uploads/'
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', '/var/www/antisocial/antisocial/sitemedia'),
-)
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'antisocial',
-        'HOST': '',
-        'PORT': 6432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
-
-COMPRESS_ROOT = os.path.join(os.path.dirname(__file__), "../media")
-DEBUG = False
-TEMPLATE_DEBUG = DEBUG
-
-AWS_S3_CUSTOM_DOMAIN = "d115djs1mf98us.cloudfront.net"
-
-AWS_STORAGE_BUCKET_NAME = "thraxil-antisocial-static-prod"
-AWS_PRELOAD_METADATA = True
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
-S3_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
-STATIC_URL = 'https://%s/media/' % AWS_S3_CUSTOM_DOMAIN
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = True
-COMPRESS_ROOT = STATIC_ROOT
-COMPRESS_URL = STATIC_URL
-DEFAULT_FILE_STORAGE = 'cacheds3storage.MediaRootS3BotoStorage'
-MEDIA_URL = S3_URL + '/media/'
-COMPRESS_STORAGE = 'cacheds3storage.CompressorS3BotoStorage'
-AWS_QUERYSTRING_AUTH = False
-
-
-if 'migrate' not in sys.argv:
-    INSTALLED_APPS = INSTALLED_APPS + [
-        'raven.contrib.django.raven_compat',
-    ]
+locals().update(common(app=app, base=base, cloudfront=cloudfront))
 
 try:
     from local_settings import *
--- antisocial/settings_shared.py
+++ antisocial/settings_shared.py
@@ -1,170 +1,40 @@
 # Django settings for antisocial project.
 import os.path
-import sys
 import djcelery
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+from thraxilsettings.shared import common
 
-ADMINS = ()
+base = os.path.dirname(__file__)
+locals().update(common(app='antisocial', base=base))
 
-MANAGERS = ADMINS
+# extended common settings. remember to '# noqa' them.
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'antisocial',
-        'HOST': '',
-        'PORT': 5432,
-        'USER': '',
-        'PASSWORD': '',
-        'ATOMIC_REQUESTS': True,
-    }
-}
+ALLOWED_HOSTS += ['.thraxil.org']  # noqa
 
-if 'test' in sys.argv or 'jenkins' in sys.argv:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': ':memory:',
-            'HOST': '',
-            'PORT': '',
-            'USER': '',
-            'PASSWORD': '',
-            'ATOMIC_REQUESTS': True,
-        }
-    }
+INSTALLED_APPS += [  # noqa
+    'userena',
+    'guardian',
+    'easy_thumbnails',
+    'bootstrapform',
+    'djcelery',
+    'typogrify',
 
-TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+    'antisocial.main',
+    'antisocial.profile',
+]
 
-JENKINS_TASKS = (
-    'django_jenkins.tasks.run_pylint',
-    'django_jenkins.tasks.run_pep8',
-    'django_jenkins.tasks.run_pyflakes',
-)
+# project specific settings
+
 PROJECT_APPS = [
     'antisocial.main',
 ]
 
-ALLOWED_HOSTS = ['localhost', '.thraxil.org']
-
-USE_TZ = True
-TIME_ZONE = 'America/New_York'
-LANGUAGE_CODE = 'en-us'
-SITE_ID = 1
-USE_I18N = False
-MEDIA_ROOT = "/var/www/antisocial/uploads/"
-MEDIA_URL = '/uploads/'
-SECRET_KEY = ')ng#)ef_u@_^zvvu@dxm7ql-yb^_!a6%v3v^j3b(mp+)l+5%@h'
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.request',
-    'stagingcontext.staging_processor',
-    'django.core.context_processors.static',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django_statsd.middleware.GraphiteRequestTimingMiddleware',
-    'django_statsd.middleware.GraphiteMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'waffle.middleware.WaffleMiddleware',
-)
-
-ROOT_URLCONF = 'antisocial.urls'
-
-TEMPLATE_DIRS = (
-    "/var/www/antisocial/templates/",
-    os.path.join(os.path.dirname(__file__), "templates"),
-)
-
-INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.flatpages',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'typogrify',
-    'compressor',
-    'bootstrapform',
-    'debug_toolbar',
-    'waffle',
-    'django_jenkins',
-    'smoketest',
-    'django_extensions',
-    'antisocial.main',
-    'userena',
-    'guardian',
-    'easy_thumbnails',
-    'antisocial.profile',
-    'djcelery',
-    'django_statsd',
-    'gunicorn',
-    'django_markwhat',
-]
-
-STATIC_URL = "/media/"
-STATICFILES_DIRS = (
-    os.path.abspath(os.path.join(os.path.dirname(__file__), "../media/")),
-)
-STATIC_ROOT = ""
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
-
-LETTUCE_APPS = (
-    'antisocial.main',
-)
-
-INTERNAL_IPS = ('127.0.0.1', )
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.template.TemplateDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.signals.SignalDebugPanel',
-)
-
-THUMBNAIL_SUBDIR = "thumbs"
-EMAIL_SUBJECT_PREFIX = "[antisocial] "
-EMAIL_HOST = 'localhost'
-SERVER_EMAIL = "antisocial@thraxil.org"
-DEFAULT_FROM_EMAIL = SERVER_EMAIL
-
-# put any static media here to override app served static media
-STATICMEDIA_MOUNTS = (
-    ('/sitemedia', 'sitemedia'),
-)
-
-COMPRESS_URL = "/media/"
-COMPRESS_ROOT = "media/"
-
-COMPRESS_CSS_FILTERS = [
-    'compressor.filters.css_default.CssAbsoluteFilter',
-    'compressor.filters.cssmin.CSSMinFilter',
-]
-
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = [
     'userena.backends.UserenaAuthenticationBackend',
     'guardian.backends.ObjectPermissionBackend',
     'django.contrib.auth.backends.ModelBackend',
-)
+]
+
 ANONYMOUS_USER_ID = -1
 AUTH_PROFILE_MODULE = 'profile.Profile'
 
@@ -172,32 +42,7 @@ LOGIN_REDIRECT_URL = '/accounts/%(username)s/'
 LOGIN_URL = '/accounts/signin/'
 LOGOUT_URL = '/accounts/signout/'
 
-SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_HTTPONLY = True
-
 BROKER_URL = "amqp://guest:guest@localhost:5672//"
 CELERYD_CONCURRENCY = 4
 
-# from datetime import timedelta
-
-# CELERYBEAT_SCHEDULE = {
-#     'schedule-feeds': {
-#         'task': 'antisocial.main.tasks.schedule_feeds',
-#         'schedule': timedelta(seconds=60),
-#         'args': (),
-#     },
-# }
-
-# CELERY_TIMEZONE = 'UTC'
-
-STATSD_CLIENT = 'statsd.client'
-STATSD_PREFIX = 'antisocial'
-STATSD_HOST = '127.0.0.1'
-STATSD_PORT = 8125
-
 djcelery.setup_loader()
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': True,
-}
