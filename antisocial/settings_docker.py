4ae929d88be99aa1ce46b7eada70c37aa621e9d6
Anders Pearson <anders@columbia.edu>
2015-09-12 22:32:39 +0200

    thraxilsettings tweaks

--- antisocial/settings_docker.py
+++ antisocial/settings_docker.py
@@ -6,4 +6,10 @@ import os.path
 app = 'antisocial'
 base = os.path.dirname(__file__)
 
-locals().update(common(app=app, base=base))
+locals().update(
+    common(
+        app=app,
+        base=base,
+        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,
+    ))
--- antisocial/settings_production.py
+++ antisocial/settings_production.py
@@ -7,7 +7,14 @@ app = 'antisocial'
 base = os.path.dirname(__file__)
 cloudfront = "d115djs1mf98us.cloudfront.net"
 
-locals().update(common(app=app, base=base, cloudfront=cloudfront))
+locals().update(
+    common(
+        app=app,
+        base=base,
+        cloudfront=cloudfront,
+        INSTALLED_APPS=INSTALLED_APPS,
+        STATIC_ROOT=STATIC_ROOT,
+    ))
 
 try:
     from local_settings import *
