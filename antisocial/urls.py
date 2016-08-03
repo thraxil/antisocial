656498f7baf0452b5dcaf93d887f1f4e254dd5f0
Anders Pearson <anders@columbia.edu>
2016-08-03 20:41:25 +0100

    Django 1.10 style urls

--- antisocial/urls.py
+++ antisocial/urls.py
@@ -1,33 +1,34 @@
-from django.conf.urls import patterns, include
+import django.views.static
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
+from antisocial.main.views import (
+    index, subscriptions, subscription, subscription_mark_read, entries,
+    unsubscribe, add_subscription, subscription_fetch, subscribe, entry_api,
+    import_feeds,
+)
 
 admin.autodiscover()
 
-urlpatterns = patterns(
-    '',
-    (r'^$', 'antisocial.main.views.index'),
-    (r'^subscriptions/$', 'antisocial.main.views.subscriptions'),
-    (r'^subscriptions/(?P<id>\d+)/$', 'antisocial.main.views.subscription'),
-    (r'^subscriptions/(?P<id>\d+)/mark_read/$',
-     'antisocial.main.views.subscription_mark_read'),
-    (r'^subscriptions/(?P<id>\d+)/fetch/$',
-     'antisocial.main.views.subscription_fetch'),
-    (r'^subscriptions/(?P<id>\d+)/unsubscribe/$',
-     'antisocial.main.views.unsubscribe'),
-    (r'^subscriptions/(?P<id>\d+)/subscribe/$',
-     'antisocial.main.views.subscribe'),
-    (r'^subscriptions/add/$', 'antisocial.main.views.add_subscription'),
-    (r'^subscriptions/import/$', 'antisocial.main.views.import_feeds'),
+urlpatterns = [
+    url(r'^$', index),
+    url(r'^subscriptions/$', subscriptions),
+    url(r'^subscriptions/(?P<id>\d+)/$', subscription),
+    url(r'^subscriptions/(?P<id>\d+)/mark_read/$', subscription_mark_read),
+    url(r'^subscriptions/(?P<id>\d+)/fetch/$', subscription_fetch),
+    url(r'^subscriptions/(?P<id>\d+)/unsubscribe/$', unsubscribe),
+    url(r'^subscriptions/(?P<id>\d+)/subscribe/$', subscribe),
+    url(r'^subscriptions/add/$', add_subscription),
+    url(r'^subscriptions/import/$', import_feeds),
 
-    (r'api/entries/$', 'antisocial.main.views.entries'),
-    (r'api/entry/(?P<id>\d+)/$', 'antisocial.main.views.entry_api'),
+    url(r'api/entries/$', entries),
+    url(r'api/entry/(?P<id>\d+)/$', entry_api),
 
-    (r'^accounts/', include('userena.urls')),
-    (r'^admin/', include(admin.site.urls)),
-    (r'smoketest/', include('smoketest.urls')),
-    (r'^stats/$', TemplateView.as_view(template_name="stats.html")),
-    (r'^uploads/(?P<path>.*)$',
-     'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
-)
+    url(r'^accounts/', include('userena.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'smoketest/', include('smoketest.urls')),
+    url(r'^stats/$', TemplateView.as_view(template_name="stats.html")),
+    url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
+        {'document_root': settings.MEDIA_ROOT}),
+]
