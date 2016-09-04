4c004edfec57ac6de82b0ab2f6e04156a2aa794b
Anders Pearson <anders@columbia.edu>
2016-09-04 23:19:41 +0100

    :arrow_up: Django 1.10

--- antisocial/settings_shared.py
+++ antisocial/settings_shared.py
@@ -22,6 +22,27 @@ INSTALLED_APPS += [  # noqa
     'antisocial.profile',
 ]
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            os.path.join(os.path.dirname(__file__), "templates"),
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 # project specific settings
 
 PROJECT_APPS = [
