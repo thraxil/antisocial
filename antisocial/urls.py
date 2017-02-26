9ec46fc86bb7be9bc269242882fdf46e1911954c
Anders Pearson <anders@columbia.edu>
2017-02-26 11:16:26 +0000

    django debug toolbar url fix

--- antisocial/urls.py
+++ antisocial/urls.py
@@ -32,3 +32,9 @@ urlpatterns = [
     url(r'^uploads/(?P<path>.*)$', django.views.static.serve,
         {'document_root': settings.MEDIA_ROOT}),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
