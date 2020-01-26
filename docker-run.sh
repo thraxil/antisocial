30fe1b4085781480c8c35644c269379961588d7f
Anders Pearson <anders@thraxil.org>
2020-01-26 15:20:13 +0000

    untabify

--- docker-run.sh
+++ docker-run.sh
@@ -9,39 +9,39 @@ else
 fi
 
 if [ "$1" == "migrate" ]; then
-		exec /ve/bin/python3 manage.py migrate --noinput
+    exec /ve/bin/python3 manage.py migrate --noinput
 fi
 
 if [ "$1" == "collectstatic" ]; then
-		exec /ve/bin/python3 manage.py collectstatic --noinput
+    exec /ve/bin/python3 manage.py collectstatic --noinput
 fi
 
 if [ "$1" == "compress" ]; then
-		exec /ve/bin/python3 manage.py compress
+    exec /ve/bin/python3 manage.py compress
 fi
 
 if [ "$1" == "shell" ]; then
-		exec /ve/bin/python3 manage.py shell
+    exec /ve/bin/python3 manage.py shell
 fi
 
 if [ "$1" == "worker" ]; then
-		exec /ve/bin/celery worker -A $APP -l info
+    exec /ve/bin/celery worker -A $APP -l info
 fi
 
 if [ "$1" == "beat" ]; then
-		exec /ve/bin/celery beat -A $APP -l info
+    exec /ve/bin/celery beat -A $APP -l info
 fi
 
 # run arbitrary commands
 if [ "$1" == "manage" ]; then
-		shift
-		exec /ve/bin/python3 manage.py "$@"
+    shift
+    exec /ve/bin/python3 manage.py "$@"
 fi
 
 
 if [ "$1" == "run" ]; then
-		exec /ve/bin/gunicorn -c /app/$APP/gunicorn_conf.py --env \
-				 DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE \
-				 $APP.wsgi:application -b 0.0.0.0:8000 -w 3 \
-				 --access-logfile=- --error-logfile=-
+    exec /ve/bin/gunicorn -c /app/$APP/gunicorn_conf.py --env \
+         DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE \
+         $APP.wsgi:application -b 0.0.0.0:8000 -w 3 \
+         --access-logfile=- --error-logfile=-
 fi
